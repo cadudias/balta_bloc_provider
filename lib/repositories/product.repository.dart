125a61729ecd1f78b7a3bc41a0cece9a9dbdc8cc
Ricardo Hofstetter <ricardo.hofstetter@redasset.com.br>
2019-08-15 17:26:14 -0300

    adicionar models e repository de produto

---
+++ lib/repositories/product.repository.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:balta_bloc_providers/models/product-details.model.dart';
+import 'package:balta_bloc_providers/models/product-list-item.model.dart';
+import '../settings.dart';
+
+class ProductRepository
+{
+  // Future é uma promessa que só é retornado quando termina a operação
+  Future<List<ProductListItemModel>> getAll() async 
+  {
+    var url = "${Settings.apiUrl}/v1/products";
+
+    Response response = await Dio().get(url);
+    
+    return (response.data as List)
+    .map((course) => ProductListItemModel.fromJson(course))
+    .toList();
+  }
+
+  Future<List<ProductDetailsModel>> getByCategory(String category) async 
+  {
+    var url = "${Settings.apiUrl}/v1/categories/$category/products";
+    
+    Response response = await Dio().get(url);
+    
+    return (response.data as List).map((course) => ProductDetailsModel.fromJson(course)).toList();
+  }
+
+  Future<ProductDetailsModel> get(String tag) async 
+  {
+    var url = "${Settings.apiUrl}/v1/products/$tag";
+    
+    Response response = await Dio().get(url);
+    
+    return ProductDetailsModel.fromJson(response.data);
+  }
+}
