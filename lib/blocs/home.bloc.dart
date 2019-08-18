5b2b73824ebdce72e56691458585a0225d7eabd8
Ricardo Dias <cadu.hd@gmail.com>
2019-08-17 21:58:15 -0300

    adicionar category card widget com home bloc

--- lib/blocs/home.bloc.dart
+++ lib/blocs/home.bloc.dart
@@ -4,6 +4,7 @@ import '../repositories/category.repository.dart';
 import '../repositories/product.repository.dart';
 import 'package:flutter/widgets.dart';
 
+// ChangeNotifier - unciona como pub/sub, se o estado alterar avisa a tela pra alterar
 class HomeBloc extends ChangeNotifier {
   final categoryRepository = new CategoryRepository();
   final productRepository = new ProductRepository();
@@ -20,21 +21,21 @@ class HomeBloc extends ChangeNotifier {
   getCategories() {
     categoryRepository.getAll().then((data) {
       this.categories = data;
-      notifyListeners();
+      notifyListeners(); // notifica que o estado mudou e a tela tem que refletir a mudança
     });
   }
 
   getProducts() {
     productRepository.getAll().then((data) {
       this.products = data;
-      notifyListeners();
+      notifyListeners(); // notifica que o estado mudou e a tela tem que refletir a mudança
     });
   }
 
   getProductsByCategory() {
     productRepository.getByCategory(selectedCategory).then((data) {
       this.products = data;
-      notifyListeners();
+      notifyListeners(); // notifica que o estado mudou e a tela tem que refletir a mudança
     });
   }
 
@@ -42,6 +43,6 @@ class HomeBloc extends ChangeNotifier {
     selectedCategory = tag;
     products = null;
     getProductsByCategory();
-    notifyListeners();
+    notifyListeners(); // notifica que o estado mudou e a tela tem que refletir a mudança
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:balta_bloc_providers/blocs/home.bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,23 +8,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
+    return MultiProvider(
+      // a aplicação passa pelo multi providers e deixa o home bloc na memoria
+      // toda a vez que o home bloc for usado ele vai usar a instancia que foi criada
+      providers: [
+        ChangeNotifierProvider<HomeBloc>.value(
+          value: HomeBloc(),
+        ),
+      ],
+      child: Main(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: Text("Shopping cart"),
+    return MaterialApp(
+      title: "Shopping cart",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
     );
   }
